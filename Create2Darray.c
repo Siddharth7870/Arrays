9dc78c7191e4becb9f4a880d201e80c7d6202d05
Sidharth7870 <ramanandkumar0770@gmail.com>
2025-02-03 17:01:55 +0530

    declare array for table

--- Create2Darray.c
+++ Create2Darray.c
@@ -1,4 +1,14 @@
 /*
 -> Practice.
-Q.46.-> Create a 2d array, storing the tables of 2 & 3.
+Q.46.->
+Create a 2d array, storing the tables of 2 & 3.
 */
+
+#include<stdio.h>
+
+int main()
+{
+    int table[2][10];
+    
+    return 0;
+}
